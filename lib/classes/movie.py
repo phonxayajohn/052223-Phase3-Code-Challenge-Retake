19f9fa849e4c2042ab86cd9ddc0e32e51331441b
John Phonxaya <phonxayajohn@gmail.com>
2023-07-20 12:19:39 -0500

    Code Challenge retake submission

--- lib/classes/movie.py
+++ lib/classes/movie.py
@@ -1,18 +1,32 @@
 class Movie:
-    
+    all = []
+
     def __init__(self, title):
         self.title = title
+        Movie.all.append(self)
 
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and len(title) > 0:
+            self._title = title
+        else:
+            raise Exception
+        
     def reviews(self):
         from classes.review import Review
-        pass
+        return [review for review in Review.all if review.movie == self]
     
     def reviewers(self):
-        pass
+        return list(set([review.viewer for review in self.reviews()]))
     
     def average_rating(self):
-        pass
+        total = sum ([review.rating for review in self.reviews()])
+        return total / len(self.reviews())
 
     @classmethod
     def highest_rated(cls):
-        pass
+        return max(cls.all, key=lambda movie: movie.average_rating())
--- lib/classes/viewer.py
+++ lib/classes/viewer.py
@@ -1,14 +1,28 @@
 class Viewer:
-    
+
+    all = []
+
     def __init__(self, username):
         self.username = username
+        Viewer.all.append(self)
 
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 6 <= len(username) <= 16:
+            self._username = username
+        else:
+            raise Exception
+        
     def reviews(self):
         from classes.review import Review
-        pass
+        return [review for review in Review.all if review.viewer == self]
     
     def reviewed_movies(self):
-        pass
+        return [review.movie for review in self.reviews()]
 
     def has_reviewed_movie(self, movie):
-        pass
+        return any(review.movie == movie for review in self.reviews() if review.viewer == self)
